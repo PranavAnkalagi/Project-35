40b3c7a6baf4d8fc66d6b53df1097e6f7b69d61f
PranavAnkalagi <73110426+PranavAnkalagi@users.noreply.github.com>
2021-01-06 21:32:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+//Create variables here
+var  dog, happyDog,foodS,database,foodStock;
+var dogImage,dogImage1;
+var database;
+function preload(){
+  dogImage=loadImage("Dog.png");
+  dogImage1=loadImage("happydog.png");
+ }
+
+//Function to set initial environment
+function setup() {
+ database=firebase.database();
+ createCanvas(500,500);
+
+ dog=createSprite(250,300,150,150);
+ dog.addImage(dogImage);
+ dog.scale=0.15;
+
+ foodStock=database.ref('Food');
+ foodStock.on("value",readStock);
+ textSize(20); 
+}
+
+// function to display UI
+function draw() {
+ background(46,139,87);
+
+ if(keyWentDown(UP_ARROW)){
+   writeStock(foodS);
+   dog.addImage(dogImage1);
+ }
+
+ drawSprites();
+ fill(255,255,254);
+ stroke("black");
+ text("Food remaining : "+foodS,170,200);
+ textSize(13);
+ text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+}
+
+//Function to read values from DB
+function readStock(data){
+ foodS=data.val();
+}
+
+//Function to write values in DB
+function writeStock(x){
+ if(x<=0){
+   x=0;
+ }else{
+   x=x-1;
+ } 
+ database.ref('/').update({
+   Food:x
+ })
+}
